ac04c85d25fa3019bb073cbe74341a9cf4acf1ef
Ondrej Sika <ondrej@ondrejsika.com>
2024-10-01 11:39:08 +0200

    feat: Create counter prod

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.3"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "200acaec-2d60-43ad-915a-e8f5352a4ba7"
+}
+
+module "generali_counter_prod" {
+  source = "git::https://gitlab.sikademo.com/generali/generali-counter-terraform.git?ref=master"
+
+  env    = "prod"
+  suffix = "ondrejsika"
+  firewall_rules = {
+    all = {
+      start_ip_address = "0.0.0.0"
+      end_ip_address   = "255.255.255.255"
+    }
+  }
+}
+
+output "generali_counter_prod" {
+  value     = module.generali_counter_prod
+  sensitive = true
+}
